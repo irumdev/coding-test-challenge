bbe4667531b00bd70c63990fd19797422d593bbd
irumdev <jklsj1252@gmail.com>
2021-12-25 23:38:44 +0900

    Enhancement : 비밀지도
    비트연산자 사용

--- kakao-blind-2018/secret-map/secret-map.py
+++ kakao-blind-2018/secret-map/secret-map.py
@@ -1,23 +1,9 @@
-def process(row, n):
-    result = list()
-
-    for i in range(n):
-        if row[0][i] == '1':
-            result.append('#')
-        elif row[1][i] == '1':
-            result.append('#')
-        else:
-            result.append(' ')
-
-    return ''.join(result)
-
-
 def solution(n, arr1, arr2):
     answer = list()
-    arr1 = list(map(lambda x: str(bin(x))[2:].zfill(n), arr1))
-    arr2 = list(map(lambda x: str(bin(x))[2:].zfill(n), arr2))
 
     for row in zip(arr1, arr2):
-        answer.append(process(row, n))
+        row = bin(row[0] | row[1])[2:]
+        row = row.rjust(n, '0').replace('1', '#').replace('0', ' ')
+        answer.append(row)
 
     return answer
